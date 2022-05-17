a6b52c6083e6a9010097f9eb20057db89cd0f59c
RED-PAND4 <a.deborah.inf@gmail.com>
2022-05-17 16:29:58 +0200

    re-add javascript

--- public/models/studente.js
+++ public/models/studente.js
@@ -3,13 +3,13 @@ var Schema = mongoose.Schema;
 
 //import mongoose from 'mongoose';
 
-const { ObjectId } = require('mongodb');
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://univents_database:password@univents.y54y3.mongodb.net/univents_database?retryWrites=true&w=majority');
 module.exports = mongoose.model('Utente', new Schema({
     name:  String,
     surname: String,
+    mail: String,
+    password: String,
     star_event_list : [{id_event: ObjectId}],
+    roles : String
 }));
    
 //const Student = mongoose.model('Student', studentSchema);
