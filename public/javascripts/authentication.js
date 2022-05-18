98caeddac00b3a247b3d5782df73af7dcb2d6b06
Elia Tomasi <elia.tomasi@pm.me>
2022-05-18 17:26:14 +0200

    .

--- public/javascripts/authentication.js
+++ public/javascripts/authentication.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const router = express.Router();
-const Utente = require('../models/utente'); // get our mongoose model
-const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+const Utente = require('../models/utente');
+const jwt = require('jsonwebtoken');
