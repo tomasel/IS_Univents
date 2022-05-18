8d00ae3106730b05653939719c914add78164eec
Elia Tomasi <elia.tomasi@pm.me>
2022-05-18 17:36:37 +0200

    First try

--- public/javascripts/authentication.js
+++ public/javascripts/authentication.js
@@ -2,3 +2,23 @@ const express = require('express');
 const router = express.Router();
 const Utente = require('../models/utente');
 const jwt = require('jsonwebtoken');
+
+let utente = await Utente.findOne({
+    email: req.body.email
+}).exec();
+
+if (!user) {
+    res.json({ success: false, message: 'Authentication failed. User not found.' });
+}
+
+if (user.password != req.body.password) {
+    res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+}
+
+res.json({
+		success: true,
+		message: 'Authentication successfull.',
+		email: user.email,
+		id: user._id,
+		self: "api/v1/" + user._id
+	});
