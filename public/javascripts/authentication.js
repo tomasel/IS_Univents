cb91b0d6c88a20faaac74244d6d18a22885185c8
Elia Tomasi <elia.tomasi@pm.me>
2022-05-20 14:45:17 +0200

    token not required

--- public/javascripts/authentication.js
+++ public/javascripts/authentication.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Utente = require('../models/utente');
-const jwt = require('jsonwebtoken');
+//const jwt = require('jsonwebtoken');
 
 router.post('', async function(req, res) {
     let utente = await Utente.findOne({
