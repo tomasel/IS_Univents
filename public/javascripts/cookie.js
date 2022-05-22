20b97cf72d6fd9ce2165742fdbc577d59833b8eb
Elia Tomasi <elia.tomasi@pm.me>
2022-05-22 15:53:32 +0200

    installed cookie-session

---
+++ public/javascripts/cookie.js
@@ -0,0 +1,11 @@
+var cookieSession = require('cookie-session')
+var express = require('express')
+
+var app = express()
+
+app.use(cookieSession({
+  name: 'session',
+  keys: [/* secret keys */],
+
+  // Cookie Options
+}))
