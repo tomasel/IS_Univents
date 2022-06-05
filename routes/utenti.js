5a8afbf47d9cecc853d13e3ddda17f6c48f52b06
RED-PAND4 <a.deborah.inf@gmail.com>
2022-06-05 10:01:22 +0200

    add api per gestione preferiti

--- routes/utenti.js
+++ routes/utenti.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const { default: mongoose } = require('mongoose');
 const Utente = require('../public/models/utente');
+const Event = require('../public/models/evento');
 const app = express();
 
 
@@ -37,41 +38,50 @@ try{
 /*PATCH add star event to user array*/
 app.patch('/:id_evento',async (req, res) => {
   const userId = mongoose.Types.ObjectId("6289f47e6fdc1cd83ca2b39a");
-
-try{
-      Utente.findOneAndUpdate(
-        { _id: userId }, 
-        { $push: { star_event_list: [ {_id:req.params.id_evento  }]  } },
-       function (error, success) {
-             if (error) {
-                 console.log(error);
-             } else {
-                 console.log(success);
-             }
-         });
-  res.status(200).json("event added");
-}catch (err) {
-  return res.status(500).send({
-    error: err || 'Something went wrong.'
-  });
-} 
+  Event.exists({_id:req.params.id_evento}, function(err, succ){
+    if (err){
+      console.log(err)
+    }else{
+      if(succ == null){return res.status(404).json("event not exist")}
+      console.log("event exist");
+      try{
+        Utente.findOneAndUpdate(
+          { _id: userId }, 
+          { $push: { star_event_list:  {_id:req.params.id_evento  }  } },
+         function (error, success) {
+               if (error) {
+                   console.log(error);
+               } else {
+                   //console.log(success);
+               }
+           });
+    res.status(200).json("event added");
+  }catch (err) {
+    return res.status(500).send({
+      error: err || 'Something went wrong.'
+    });
+  }
+    }
+    });
+ 
 });
 
 /*DELETE  star event in user array*/
 app.delete('/:id_evento',async (req, res) => {
   const userId = mongoose.Types.ObjectId("6289f47e6fdc1cd83ca2b39a");
-
+  
 try{
       Utente.findOneAndUpdate(
         { _id: userId }, 
-        { $pullAll: { star_event_list: [ {_id:req.params.id_evento  }] }},
-       function (error, success) {
-             if (error) {
-                 console.log(error);
-             } else {
-                 console.log(success);
-             }
-         });
+        { $pull: { star_event_list:  {_id:req.params.id_evento  } }},
+        function (error, success) {
+          if (error) {
+              console.log(error);
+          } else {
+              //console.log(success);
+          }
+      });
+      
   res.status(200).json("event deleted");
 }catch (err) {
   return res.status(500).send({
