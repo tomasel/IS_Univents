9421db210d2327f0dec14200fca24f1338222ed1
Elia Tomasi <elia.tomasi@pm.me>
2022-06-06 16:08:14 +0200

    restricted return values

--- routes/authentication.js
+++ routes/authentication.js
@@ -7,15 +7,13 @@ app.post ('', async function(req, res) {
     console.log("looking for user");
     let user = await Utente.findOne({
         email: req.body.email
-    });
-    console.log("found user "+user);
+    },'name email password roles');
 
     if (user == null) {
         console.log("user is null");
         return res.status(500).json({ success: false, message: 'Authentication failed. User not found.' });
     }
 
-    console.log(user.password+req.body.password);
     if (user.password != req.body.password) {
         console.log("no pass");
         return res.status(500).json({ success: false, message: 'Authentication failed. Wrong password.' });
@@ -29,6 +27,7 @@ app.post ('', async function(req, res) {
 	var options = {
 		expiresIn: 86400 
 	}
+    // SUPER_SECRET
 	var token = jwt.sign(payload, "a726f6a47a7757160e105748c720435fc508311528585f5601f2d8dbaae60ade", options);
 
     res.json({
@@ -37,10 +36,10 @@ app.post ('', async function(req, res) {
             email: user.email,
             id: user._id,
             token: token,
-            self: "/home" + user._id + "&" + token
+            self: '/home' + user.id
         });
         
-        //console.log(user + token + payload);
+        console.log("end of auth");
 });
 
 module.exports = app;
