4c328309012f2cf061a1f29cf89f52b8c69c34c9
Elia Tomasi <elia.tomasi@pm.me>
2022-06-06 15:39:06 +0200

    now it works (kinda)

--- routes/authentication.js
+++ routes/authentication.js
@@ -4,16 +4,21 @@ const jwt = require('jsonwebtoken');
 const app = express();
 
 app.post ('', async function(req, res) {
+    console.log("looking for user");
     let user = await Utente.findOne({
         email: req.body.email
     });
+    console.log("found user "+user);
 
-    if (!user) {
-        res.status(500).json({ success: false, message: 'Authentication failed. User not found.' });
+    if (user == null) {
+        console.log("user is null");
+        return res.status(500).json({ success: false, message: 'Authentication failed. User not found.' });
     }
 
+    console.log(user.password+req.body.password);
     if (user.password != req.body.password) {
-        res.status(500).json({ success: false, message: 'Authentication failed. Wrong password.' });
+        console.log("no pass");
+        return res.status(500).json({ success: false, message: 'Authentication failed. Wrong password.' });
     }
 
     // crea token
@@ -24,7 +29,7 @@ app.post ('', async function(req, res) {
 	var options = {
 		expiresIn: 86400 
 	}
-	var token = jwt.sign(payload, process.env.SUPER_SECRET, options);
+	var token = jwt.sign(payload, "a726f6a47a7757160e105748c720435fc508311528585f5601f2d8dbaae60ade", options);
 
     res.json({
             success: true,
@@ -34,6 +39,8 @@ app.post ('', async function(req, res) {
             token: token,
             self: "/home" + user._id + "&" + token
         });
+        
+        //console.log(user + token + payload);
 });
 
 module.exports = app;
